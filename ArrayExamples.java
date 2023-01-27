a66e1dc7956365d24e3698ac2de831c9620997af
shambhavi-20 <shmittal@ucsd.edu>
2023-01-26 22:40:38 -0800

    change

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -1,4 +1,4 @@
-
+import static org.junit.Assert.fail;
 
 public class ArrayExamples {
 
@@ -14,9 +14,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
@@ -29,8 +29,15 @@ public class ArrayExamples {
       if(num < lowest) { lowest = num; }
     }
     double sum = 0;
+    boolean check = false;
     for(double num: arr) {
-      if(num != lowest) { sum += num; }
+      
+      if(num == lowest && check==false){
+        check=true;
+      } 
+      else{
+         sum += num; 
+        }
     }
     return sum / (arr.length - 1);
   }
--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+
+  @Test 
+	public void testAverageWithoutLowest1() {
+    double[] input1 = {9.0,8.0,7.0,6.0};
+    double output = ArrayExamples.averageWithoutLowest(input1);
+    assertEquals(8.0, output,0);
+  }
+  @Test 
+	public void testAverageWithoutLowest2() {
+    double[] input1 = {6,6,6,7,8};
+    double output = ArrayExamples.averageWithoutLowest(input1);
+    assertEquals(6.75, output,0);
   }
 }
